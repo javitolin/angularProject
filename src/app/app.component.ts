442cc01625c3adf35ffd0cc0f8b0a9a338c9adbc
JD <JD@WOW.com>
2018-07-01 17:15:57 +0300

    Simple navigation using data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  show_recipes: boolean = true;
+  show_shopping_list: boolean = false;
 
+  OnMenuItemChosen(menu_clicked: string) {
+    if (menu_clicked === 'recipes') {
+      this.show_recipes = true;
+      this.show_shopping_list = false;
+    } else {
+      this.show_shopping_list = true;
+      this.show_recipes = false;
+    }
+  }
 }
